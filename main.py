6e198614cb969c59edfd6d3cc77d508d7210d7ca
Mateo <matusmaldonado@gmail.com>
2024-12-13 12:57:33 -0300

    configurando app documentacion

--- main.py
+++ main.py
@@ -2,11 +2,21 @@ from model.team import Team
 from model.hero import Hero 
 from repository.repository_team import TeamRepository
 from repository.repository_hero import HeroRepository
+from fastapi import FastAPI, HTTPException, Depends
+from pydantic import BaseModel
+app = FastAPI()
 
-
-
-team_repo = TeamRepository()
 hero_repo = HeroRepository()
+
+
+@app.get("/")
+def hola_mundo():
+    return {"hello":"world"}
+
+@app.post("/")
+def create_heroe():
+    return {"hello":"world"}
+
 
 #hero_2 = Hero(name="Spider-Boy", secret_name="Pedro Parqueador")
 #hero_3 = Hero(name="Rusty-Man", secret_name="Tommy Sharp", age=48)
